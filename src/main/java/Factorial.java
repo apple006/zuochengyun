c5e8d12c10f1bb296d1cf3cba36d852c74efeebc
ddcv587 <ddcv587>
2017-02-12 18:40:34 +0800

    特殊的斐波那契

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -16,6 +16,11 @@ public class Factorial {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Factorial().math(3));
+        int x=6,y=8;
+        boolean b;
+        b=x<y || ++x==--y;
+        System.out.println(b);
+        System.out.println(x);
+        System.out.println(y);
     }
 }
